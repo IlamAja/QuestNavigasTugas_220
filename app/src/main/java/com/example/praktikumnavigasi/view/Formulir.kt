0889a9c6b544ccf95b5e4fc4b2e1fc815e2ccee5
Ilam Ball <informasiilam@gmail.com>
2025-11-07 17:58:21 +0700

    mengubah halaman formulir

--- app/src/main/java/com/example/praktikumnavigasi/view/Formulir.kt
+++ app/src/main/java/com/example/praktikumnavigasi/view/Formulir.kt
@@ -4,94 +4,52 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.praktikumnavigasi.R
+import com.example.praktikumnavigasi.ui.theme.PraktikumNavigasiTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
-    onSubmitBtnClick : () -> Unit
-){
-    Scaffold (
-        modifier = Modifier,
+    onSubmitBtnClick: (String, String, String, String) -> Unit,
+    onBackBtnClick: () -> Unit
+) {
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+
+    val lightPurple = Color(0xFFF3E5F5)
+    val darkPurple = Color(0xFF8E44AD)
+
+    Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(stringResource(id = R.string.home)) },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = colorResource(id = R.color.teal_200),
-                )
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = darkPurple)
             )
-        }
-    ) {
-            isiRuang ->
-        Column (modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(250.dp),
-                label = { Text(text = "Nama Lengkap") },
-                onValueChange = {},
-            )
-            HorizontalDivider(modifier = Modifier
-                .padding(20.dp)
-                .width(250.dp), thickness = 1.dp,color = Color.Red)
-            Row{
-                jenisK.forEach {
-                        item->
-                    Row(verticalAlignment = Alignment.CenterVertically){
-                        RadioButton(
-                            selected = false,
-                            onClick = {}
-                        )
-                        Text(text = item)
-                    }
-                }
-            }
-            HorizontalDivider(modifier = Modifier
-                .padding(20.dp)
-                .width(250.dp),
-                thickness = 1.dp,
-                color = Color.Red
-            )
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .width(250.dp),
-                label = {Text(text = "Alamat")},
-                onValueChange = {},
-            )
-            Spacer(modifier = Modifier.height(30.dp))
-            Button(
-                modifier = Modifier.fillMaxWidth(1f),
-                onClick = onSubmitBtnClick
-            ){
-                Text(stringResource(id= R.string.submit))
-            }
-        }
-    }
-}
+        },
