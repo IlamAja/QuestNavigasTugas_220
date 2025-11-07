88765e4f1a7bccc2b4341081e39b430d79646812
Ilam Ball <informasiilam@gmail.com>
2025-11-07 17:06:49 +0700

    feat: membuat halaman beranda

--- app/src/main/java/com/example/praktikumnavigasi/view/Beranda.kt
+++ app/src/main/java/com/example/praktikumnavigasi/view/Beranda.kt
@@ -1,2 +1,42 @@
 package com.example.praktikumnavigasi.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.praktikumnavigasi.R
+import com.example.praktikumnavigasi.ui.theme.PraktikumNavigasiTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanBeranda(
+    onNextButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val lightPurple = Color(0xFFF3E5F5)
+    val darkPurple = Color(0xFF8E44AD)
+
