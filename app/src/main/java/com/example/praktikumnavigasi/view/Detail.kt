7a3d5d2e42517fa4f452b1a21e93cee18e3a681b
Ilam Ball <informasiilam@gmail.com>
2025-11-07 17:53:59 +0700

    memperbarui halaman detail

--- app/src/main/java/com/example/praktikumnavigasi/view/Detail.kt
+++ app/src/main/java/com/example/praktikumnavigasi/view/Detail.kt
@@ -3,73 +3,48 @@ package com.example.praktikumnavigasi.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.praktikumnavigasi.DataUiState
 import com.example.praktikumnavigasi.R
+import com.example.praktikumnavigasi.ui.theme.PraktikumNavigasiTheme
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
-    onBackBtnClick:() -> Unit
+    dataUiState: DataUiState,
+    onBackToBerandaClick: () -> Unit,
+    onBackToFormClick:() -> Unit
 ){
-    val items = listOf(
-        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
-        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
-        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
-    )
+    val lightPurple = Color(0xFFF3E5F5)
+    val darkPurple = Color(0xFF8E44AD)
+
     Scaffold (
-        modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = {Text(text = stringResource(id=R.string.tampil), color = Color.White)},
-                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+                title = {Text(text = "List Daftar Peserta", color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = darkPurple)
             )
-        }
-    ){     isiRuang->
-        Column(modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.SpaceBetween) {
-            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))) {
-                items.forEach { item ->
-                    Column {
-                        Text(
-                            text = item.first.uppercase(),
-                            fontSize = 16.sp
-                        )
-                        Text(
-                            text = item.second,
-                            fontWeight = FontWeight.Bold,
-                            fontFamily = FontFamily.Cursive, fontSize = 22.sp
-                        )
-                    }
-                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
-                }
-                Spacer(modifier = Modifier.height(10.dp))
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = onBackBtnClick
-                ) {
-                    Text(text = stringResource(id = R.string.back))
-                }
-            }
-        }
-    }
-}
+        },
