f24bab39973072aeaf4e933050fd54e7ddbe5052
Ilam Ball <informasiilam@gmail.com>
2025-11-07 15:08:49 +0700

    menambahkan file navigasi untuk navigasi yang sedang dikerjakan

---
+++ app/src/main/java/com/example/praktikumnavigasi/Navigasi.kt
@@ -0,0 +1,51 @@
+package com.example.praktikumnavigasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikumnavigasi.view.FormIsian
+import com.example.praktikumnavigasi.view.TampilData
+
+enum class Navigasi {
+    Formulir,
+
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = modifier.padding(paddingValues = isiRuang)
+        ){
+            composable(route = Navigasi.Formulir.name){
+                FormIsian (
+                    onSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {cancelAndBackToFormulir(navController)}
+                )
+            }
+        }
+    }
+}
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(Navigasi.Formulir.name, inclusive = false)
+}
